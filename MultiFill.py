a66ffda962a2ae5d0451b1bb7106dd1e261771d4
Lellansin <lellansin@gmail.com>
2013-08-10 17:40:04 +0800

    init commit

---
+++ MultiFill.py
@@ -0,0 +1,100 @@
+
+import sublime, sublime_plugin
+import os, random;
+
+# 
+#	inner function
+# 
+
+def nums(i):
+	return str(i)
+
+def nums2(i):
+	return str(i+1)
+
+def upAlphabet(i):
+	return chr(i+65)
+
+def lwAlphabet(i):
+	return chr(i+97)
+
+func = [nums, nums2, upAlphabet, lwAlphabet]
+
+def userCustom(order, data, length, pos):
+	if (order == 'ordered'):
+		return data[pos]
+	elif (order == 'random'):
+		return data[random.randint(0,length-1)]
+
+
+######################################################################
+
+# 	text insert part
+
+######################################################################
+
+def getList():
+	return [ 'Numners since 0 (ordered)' ,'Numners since 1 (ordered)' ,'Uppercase letters (ordered)' ,'Lower case letters (ordered)' ];
+
+class MultiFillSetTextCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		global func
+		tmp_settings = sublime.load_settings('MultiFill.tmp')
+		chosen = tmp_settings.get('chosen')
+		if (chosen>3):
+			global settings
+			custom = settings.get("custom")
+			item = custom[chosen - len(func)]
+			points = self.view.sel()
+			count = len(points)
+			for i in xrange(0, count):
+				# echo(str(len(item["values"])));
+				self.view.replace(edit, points[i], userCustom(item['way'], item["values"], len(item["values"]), i))
+		else:			
+			points = self.view.sel()
+			count = len(points)
+			for i in xrange(0, count):
+				self.view.replace(edit, points[i], func[chosen](i))
+
+######################################################################
+
+#	select panel
+
+######################################################################
+
+def getCustom():
+	global settings
+	settings = sublime.load_settings('MultiFill.sublime-settings')
+	custom = settings.get("custom")
+	result = []
+	for i in xrange(0,len(custom)):
+		result += [ custom[i]["name"] ];
+	return result
+
+class MultiFillCommand(sublime_plugin.WindowCommand):
+	def run(self):
+		self.command_list = getList() + getCustom();
+		if not self.command_list:
+			echo('There may be some error with the config file.')
+			return
+		self.window.show_quick_panel(self.command_list, self.on_done)
+
+	def on_done(self, index):
+		if (index == -1):
+			return
+
+		settings = sublime.load_settings('MultiFill.tmp')
+		settings.set('chosen', index)
+		c = settings.get('chosen')
+		sublime.save_settings('MultiFill.tmp')
+
+		view = sublime.active_window().active_view()	
+		if view:
+			view.run_command('multi_fill_set_text')
+
+# 
+# 	debug
+# 
+
+def echo(str):
+	sublime.message_dialog(str);
---
+++ README.md
@@ -0,0 +1,65 @@
+MultiFill
+====================
+
+A sublime text plugin to multi-fill text.
+
+With this plugin, you can press <code>[ctrl+m]</code> and <code>[ctrl+k]</code>, when you use the sublime choose multi-place, to select a text type to fill multi-place automatically.
+
+Um, I think it will be very useful, while we write .html .sql and so on.
+
+
+<h1>Using Screenshot</h1>
+
+<h2>Ordered fill</h2>
+
+![image](https://github.com/Lellansin/MultiFill/raw/master/screenshots/multi_fill_names_1.jpg)
+![image](https://github.com/Lellansin/MultiFill/raw/master/screenshots/multi_fill_names_2.jpg)
+![image](https://github.com/Lellansin/MultiFill/raw/master/screenshots/multi_fill_names_3.jpg)
+
+<h2>Random fill</h2>
+
+![image](https://github.com/Lellansin/MultiFill/raw/master/screenshots/multi_fill_sex_1.jpg)
+![image](https://github.com/Lellansin/MultiFill/raw/master/screenshots/multi_fill_sex_2.jpg)
+![image](https://github.com/Lellansin/MultiFill/raw/master/screenshots/multi_fill_sex_3.jpg)
+
+<h1>Customize easily</h1>
+
+you can easily customize the MultiFill's text type title and content as you wish.
+
+the config file is <code>MultiFill.sublime-settings</code> it seems like:
+<pre>
+{
+	"custom":
+	[
+		{
+			"name"  : "Names (ordered)",
+			"way"   : "ordered",
+			"values": 
+			[
+				"Alan","Bob","Cici","David","Elisabeth","Franklin"
+			]
+		},
+		{
+			"name"  : "Gender (random)",
+			"way"   : "random",
+			"values": 
+			[
+				"male","female"
+			]
+		},
+		{
+			"name":"V Roman (ordered)",
+			"way"   : "ordered",
+			"values":
+			[
+				"I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII"
+			]
+		}			
+	]
+}
+</pre>
+
+
+<h1>Wait Help</h1>
+
+Um, I must tell you a truth that today is the second day I use python (that's really an amazing language XD), and the code I wrote seems bad, so if is okay could you help to improve this plugin, thanks a lot XD
