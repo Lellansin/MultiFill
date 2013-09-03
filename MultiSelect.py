10ae11454759f99b77c746e7e7e7f7c195f908dd
Lellansin <lellansin@gmail.com>
2013-09-03 11:17:13 +0800

    Now you can free your ctrl + D
    We will not be cautious any more while we use [ctrl+d] to make  multiple
    selections. we can use the [ctrl+alt+d] to save the points and
    selections now, and use [ctrl+alt+enter] to restore all of it.

---
+++ MultiSelect.py
@@ -0,0 +1,61 @@
+
+import sublime
+import sublime_plugin
+
+class MultiSelectCommand(sublime_plugin.WindowCommand):
+    points = {};
+    def run(self, **args):
+        view = self.window.active_view();
+        view_id = view.id();
+        points_new = list(view.sel());
+
+        # if it's the first
+        if ( not MultiSelectCommand.points.has_key(view_id) ):
+            # first save
+            MultiSelectCommand.points.setdefault(view_id, points_new);
+        else:
+            # judge if it's too many points
+            saved_num = len(MultiSelectCommand.points[view_id])
+            to_saved = len(points_new)
+            if ((saved_num + to_saved) > 32):
+            	echo('There are to many points and selections saved and this may case some problem. If you trust the performance of you CPU and RAM, you can change the limit of the MultiFill config file')
+                return
+            # save new points
+            MultiSelectCommand.points[view_id][0:0] = points_new;
+
+        # mark all the points saved
+        mark = [s for s in points_new]
+        view.add_regions("mark", MultiSelectCommand.points[view_id], "mark", "dot",
+            sublime.HIDDEN | sublime.PERSISTENT)
+
+
+class MultiSelectAllCommand(sublime_plugin.TextCommand):
+    def run(self, edit, **args):
+        view_id = self.view.id();
+
+        if ( not MultiSelectCommand.points.has_key(view_id) ):
+            print "MultiFill : No points saved"
+            return;
+
+        # get points data
+        points = MultiSelectCommand.points[view_id] 
+
+        # restore all the points and selections
+        count = len(points)
+        for i in range(0, count):
+            self.view.sel().add(points[i])
+
+        # clear the mark
+        self.view.add_regions("mark", [s for s in points], "", "", sublime.HIDDEN | sublime.PERSISTENT)
+        # clear the data
+        del MultiSelectCommand.points[view_id]
+
+
+
+
+
+#
+#   debug
+#
+def echo(message):
+    sublime.message_dialog(message)
